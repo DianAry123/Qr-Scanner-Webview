385575b4498f48074f0456a47d0eaff1d63c8945
DianAry123 <komangdianary23@gmail.com>
2020-06-20 02:59:29 +0800

    qr scanner dan web view

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import './webview.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String kode = "";
+  var getKode;
+  Future scanBarcode() async {
+    getKode = await BarcodeScanner.scan();
+    setState(() {
+      kode = getKode.rawContent;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR CODE AND WEBVIEW"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 10,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16.0),
+                bottomRight: Radius.circular(16.0),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Image.asset(
+              "assets/code.png"),
+          SizedBox(
+            height: 10.0,
+          ),
+          FlatButton(
+            onPressed: () {
+              scanBarcode();
+            },
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    "TAP TO SCAN QR CODE",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+            ),
+          ),
+          FlatButton(
+            onPressed: kode == ""
+                ? () {}
+                : () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WebScreen(kode),
+                      ),
+                    );
+                  },
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: kode == ""
+                      ? Text(
+                          "RESULT",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        )
+                      : Text(
+                          kode,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
